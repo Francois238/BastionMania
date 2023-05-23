7be1151049a699965438735822ea35ea79beff59
Francois <francois.benet@isen.yncrea.fr>
2023-05-23 10:49:14 +0200

    refonte login

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { AuthAdminGuard } from './admin/auth-admin.guard';
 import { AuthUserGuard } from './user/auth-user.guard';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full',
+  },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AuthAdminGuard] },
   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule), canActivate: [AuthUserGuard] }
